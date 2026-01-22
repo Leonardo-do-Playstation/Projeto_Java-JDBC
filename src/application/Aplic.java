3074fbce67d643bab5154b66e7f8059f3fad93b4
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-22 19:15:15 -0300

    Create Seller class

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Aplic {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(20, "Roberto", "Roberto@gmail.com", new Date(), 3000.0, obj);
 
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
