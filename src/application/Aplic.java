39d95dc1d54ddaa65dd80d7acad0d2bf50a39fd2
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-23 14:57:48 -0300

    Implement delete method

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.util.Date;
 import java.util.List;
 
@@ -30,17 +29,21 @@ public class Aplic {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "João Silva", "joao@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Insert! New id inserted is: " + newSeller.getId());
-		
+
 		System.out.println("\n==== TEST 5: seller update ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Seller updated: New name is " + seller.getName());
-		}
-	}
 
+		System.out.println("\n==== TEST 6: seller update ====");
+		seller = sellerDao.findById(newSeller.getId());
+		sellerDao.deleteById(seller.getId());
+		System.out.println("It´s done. Id exclued is " + seller.getId());
+	}
+}
