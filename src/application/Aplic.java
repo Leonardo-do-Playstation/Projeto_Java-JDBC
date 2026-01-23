aae3ddbcebceba302f5d350318917dae7dc55039
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-23 11:38:26 -0300

    FindbyId method implementation

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Aplic {
 
-	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(20, "Roberto", "Roberto@gmail.com", new Date(), 3000.0, obj);
-
+	public static void main(String[] args) {		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(1);
+		
 		System.out.println(seller);
 	}
 
